1383fadf0df976e94feab2175c4fdbd2931bcb09
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-01-11 14:15:08 +0300

    add login window

--- SwiftUILessons/ContentView.swift
+++ SwiftUILessons/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var login = ""
+    @State private var password = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("back")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .scaledToFill()
+            VStack {
+                Image("world")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                TextField("Login", text: $login)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(maxWidth: 250)
+                SecureField("Password:", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(maxWidth: 250)
+                Button(action: loginAction) {
+                    Text("Log in")
+                        .padding()
+                        .frame(width: 250, height: 50, alignment: .center)
+                }
+            }
         }
-        .padding()
+    }
+
+    private func loginAction() {
+        print("\(login) logged in")
     }
 }
 
