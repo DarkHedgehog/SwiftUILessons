38cd923a57ad8c5dd0b7454b67d81d2e0997c87e
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-01-11 13:36:09 +0300

    Initial Commit

---
+++ SwiftUILessons/SwiftUILessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILessonsApp.swift
+//  SwiftUILessons
+//
+//  Created by Aleksandr Derevenskih on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
